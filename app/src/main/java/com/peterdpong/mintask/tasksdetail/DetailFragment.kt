e2b51482bd7f6929231311fab05e567730d939e0
Soraphoj(Peter) Damrongpiriyapong <peterdpong@gmail.com>
2020-06-06 16:21:16 -0400

    Fixed animation

--- app/src/main/java/com/peterdpong/mintask/tasksdetail/DetailFragment.kt
+++ app/src/main/java/com/peterdpong/mintask/tasksdetail/DetailFragment.kt
@@ -1,10 +1,12 @@
 package com.peterdpong.mintask.tasksdetail
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.transition.MaterialArcMotion
 import com.google.android.material.transition.MaterialContainerTransform
 
 import com.peterdpong.mintask.R
@@ -22,7 +24,12 @@ class DetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sharedElementEnterTransition = MaterialContainerTransform(requireContext())
+        sharedElementEnterTransition = MaterialContainerTransform(requireContext()).apply{
+            pathMotion = MaterialArcMotion()
+            fadeMode = MaterialContainerTransform.FADE_MODE_CROSS
+            duration = 500
+            containerColor = Color.WHITE
+        }
     }
 
 }
